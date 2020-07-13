117f2b4f5da5a904302590158acae617ddb8e922
Jacob Shore <jshore@datalign.net>
2020-07-13 15:25:11 +0300

    sort test

--- main.dart
+++ main.dart
@@ -2,14 +2,21 @@ import 'Person.dart';
 
 void main() {
   List<Person> people = [eliyahu, jacob, chani];
+  people.add(ay);
+  people.add(yosef);
+
+  people.sort((a, b) => a.birthday.compareTo(b.birthday));
 
   for (Person p in people) {
     p.printAge();
-    print(p.canDrink());
+    print(p.drinkString());
     print(p.fartSmeller());
   }
 }
 
 Person eliyahu = Person("Eliyahu", "Shore", DateTime(2010, DateTime.july, 28));
 Person jacob = Person("Jacob", "Shore", DateTime(1984, DateTime.july, 5));
+Person ay =
+    Person("Asher Yitzchok", "Shore", DateTime(2016, DateTime.january, 29));
+Person yosef = Person("Yosef", "Shore", DateTime(2014, DateTime.april, 26));
 Person chani = Person("Chani", "Shore", DateTime(1984, DateTime.december, 31));
